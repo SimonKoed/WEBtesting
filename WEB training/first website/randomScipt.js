b295c16ea7d4b5ee9262283e1b86cd538ee8bf67
SimonKoed <31965808+SimonKoed@users.noreply.github.com>
2019-03-31 14:48:40 +0200

    Exercises for advanced loops completed

--- WEB training/first website/randomScipt.js
+++ WEB training/first website/randomScipt.js
@@ -392,4 +392,61 @@ for (item in detailedBasket) {
 	console.log(item);
 }
 
-//git test
+//Exercises
+
+// Question #1:
+// create a function called biggestNumberInArray() that takes
+// an array as a parameter and returns the biggest number.
+// biggestNumberInArray([-1,0,3,100, 99, 2, 99]) should return 100;
+// Use at least 3 different types of javascript loops to write this:
+const array = [-1,0,3,100, 99, 2, 99] // should return 100
+const array2 = ['a', 3, 4, 2] // should return 4
+const array3 = [] // should return 0
+
+function biggestNumberInArray(arr) {
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log(Math.max(arr));
+  }
+}
+
+  function biggestNumberInArray2(arr) {
+  let highest = 0;
+  arr.forEach(item => {
+    if (highest < item) {
+      highest = item;
+    }
+  })
+  return highest;
+}
+
+
+function biggestNumberInArray3(arr) {
+  let highest = 0;
+  for (item of arr) {
+    if (highest < item) {
+      highest = item;
+    }
+  }
+  return highest;
+}
+
+
+// Question #2:
+// Write a function checkBasket() that lets you know if the item is in the basket or not
+amazonBasket = {
+  glasses: 1
+  books: 2
+  floss: 100
+}
+
+function checkBasket(basket, lookingFor) {
+  for (item in basket) {
+    if (item === lookingFor) {
+      return `${lookingFor} is in your basket`
+    }
+  }
+  return 'that does not exist in your basket'
+}
+
+// End of exercises
